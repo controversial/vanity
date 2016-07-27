80cd8ac67b5048dab8754a222283ba56fb674606
Luke Deen Taylor <ldt@deentaylor.com>
2016-07-27 08:49:30 -0400

    Print additions by repo

--- linecount.py
+++ linecount.py
@@ -25,18 +25,18 @@ def get_my_repos():
     # Continue paginating until we hit a page with less than the maximum number
     # of entries
     while True:
-        print("Reading page {} of user repos".format(page + 1))
+        print("- Reading page {} of user repos...".format(page + 1))
         page_repos = get("user/repos", {"page": page, "per_page": 100})
         repos.extend(page_repos)
         if len(page_repos) <= 100:
             break
-
+    print("- Listed repos successfully!")
     return [tuple(repo["full_name"].split("/")) for repo in repos]
 
 
 def get_my_additions(owner, name):
-    print("Reading additions to {}/{}...".format(owner, name))
     contributions = get("repos/{}/{}/stats/contributors".format(owner, name))
+
     # Find the authenticated user's contributions
     try:
         my_contributions = [
@@ -46,13 +46,22 @@ def get_my_additions(owner, name):
     except IndexError:
         # The user has made no contributions to this repo
         total = 0
-    print("{} lines".format(total))
     return total
 
 
 def get_total_additions():
-    return sum(get_my_additions(owner, name) for owner, name in get_my_repos())
+    repos = get_my_repos()
+    print("- Reading additions...")
+    by_repo = {}
+    for owner, name in repos:
+        by_repo["{}/{}".format(owner, name)] = get_my_additions(owner, name)
+    return sum(by_repo.values()), by_repo
 
 
 if __name__ == "__main__":
-    print(get_total_additions())
+    adds = get_total_additions()
+    print("\n\n\nTotal additions: ", adds[0])
+    print("----------------------------------")
+    print("REPOS ----------------------------")
+    for name, lines in sorted(adds[1].items(), key=lambda x: x[1])[::-1]:
+        print(name+":", lines, "lines | ", end="")
